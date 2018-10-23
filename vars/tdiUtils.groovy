0d87243484df5084d2c5fbb57716c2c47803aa51
zachglassman <zach.glassman@gmail.com>
2018-10-23 09:16:58 -0700

    add ability to put extra strings in sendJenkins

--- vars/tdiUtils.groovy
+++ vars/tdiUtils.groovy
@@ -10,16 +10,16 @@ String cronString(String c) {
     }
 }
 
-void sendJenkins(boolean success) {
+void sendJenkins(boolean success, String s = '') {
     //only send if on master
     if (isMasterBranch()){
         if (success){
             // don't send if not previously bad
             if (currentBuild.getPreviousBuild().result != 'SUCCESS'){
-                slackSend(color: "good", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} is back to success ${env.BUILD_URL}")
+                slackSend(color: "good", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} is back to success ${env.BUILD_URL}\n $s")
             }
         } else { //send if not success
-            slackSend(color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was not successful, ${env.BUILD_URL}")
+            slackSend(color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was not successful, ${env.BUILD_URL} \n $s")
         }
     }
 }
