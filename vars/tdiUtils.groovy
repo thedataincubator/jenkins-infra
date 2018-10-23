0b9f84710efde2fc56866377dd5533d15142306c
zachglassman <zach.glassman@gmail.com>
2018-10-22 19:56:12 -0700

    add other utilities

--- vars/tdiUtils.groovy
+++ vars/tdiUtils.groovy
@@ -1,3 +1,29 @@
 Boolean isMasterBranch() {
     return env.JOB_NAME.split("/").last() == "master"
 }
+
+String cronString(String c) {
+    if (isMasterBranch()) {
+        return 'c'
+    } else {
+        return ''
+    }
+}
+
+void sendJenkins(boolean success) {
+    //only send if on master
+    if (isMasterBranch()){
+        if (success){
+            // don't send if not previously bad
+            if (currentBuild.getPreviousBuild().result != 'SUCCESS'){
+                slackSend(color: "good", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} is back to success ${env.BUILD_URL}")
+            }
+        } else { //send if not success
+            slackSend(color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was not successful, ${env.BUILD_URL}")
+        }
+    }
+}
+
+Boolean isBranchTriggered() {
+    return (currentBuild.rawBuild.getCauses()[0].getClass() == jenkins.branch.BranchEventCause)
+}
