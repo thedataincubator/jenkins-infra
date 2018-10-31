f718ab169830aa77e9c35136dd989e1ecb26a99d
zachglassman <zach.glassman@gmail.com>
2018-10-31 09:58:59 -0700

    add clean step

---
+++ vars/clean.groovy
@@ -0,0 +1,11 @@
+def cleanDocker(String name = ''){
+    if (name != ''){
+        sh('docker rmi $(docker images | grep ${name} | awk \'{print$3}\') || true')
+    }
+}
+
+def call(String name = ''){
+    sh 'docker container prune -f'
+    sh 'docker image prune -f'
+    cleanDocker(name)
+}
